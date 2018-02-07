108295917e793e168e064a938539193c54a79f93
agus.sustian <agus.sustian@gmail.com>
2018-02-07 15:14:15 +0700

    update timer sh

--- DS_D.sh
+++ DS_D.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+for (( i=77; i>0; i--)); do
+  sleep 1 &
+  printf "as@epic  $i \r"
+  wait
+done
 "D:\DS\kill_DS.exe" $1 &
 git pull
 "D:\DS\app.exe" $1 &
--- DS_E.sh
+++ DS_E.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+for (( i=77; i>0; i--)); do
+  sleep 1 &
+  printf "as@epic  $i \r"
+  wait
+done
 "E:\DS\kill_DS.exe" $1 &
 git pull
 "E:\DS\app.exe" $1 &
